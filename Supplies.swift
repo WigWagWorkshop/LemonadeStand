b630216a6b22e9f37da853985fb8ec8eb2fee53d
Steven <steven@WigWagWorkshop.local>
2015-01-06 00:43:41 -0500

    Updated IBActions

---
+++ Supplies.swift
@@ -0,0 +1,23 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by The Pez on 1/3/15.
+//  Copyright (c) 2015 WigWagWorkshop. All rights reserved.
+//
+
+import Foundation
+
+struct Supplies {
+    
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    init(aMoney: Int, aLemons: Int, aIceCubes: Int) {
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aIceCubes
+        
+    }
+}
